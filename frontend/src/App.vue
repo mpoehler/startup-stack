601de2c1215f1ed07a26752e7924bd765d27ac01
Marco Poehler <marco@kontaktlinsen-preisvergleich.de>
2018-09-04 16:52:18 +0200

    added material-design-components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <mdc-layout-app id="app">
+
+   <mdc-toolbar slot="toolbar">
+     <mdc-toolbar-row>
+       <mdc-toolbar-section align-start >
+         <mdc-toolbar-menu-icon event="toggle-drawer"></mdc-toolbar-menu-icon>
+         <mdc-toolbar-title>Drawer Example</mdc-toolbar-title>
+       </mdc-toolbar-section>
+     </mdc-toolbar-row>
+   </mdc-toolbar>
+
+   <mdc-drawer slot="drawer" toggle-on="toggle-drawer">
+     <mdc-drawer-list>
+       <mdc-drawer-item start-icon="inbox">Inbox</mdc-drawer-item>
+       <mdc-drawer-item start-icon="send">Sent Mail</mdc-drawer-item>
+       <mdc-drawer-item start-icon="drafts">Drafts</mdc-drawer-item>
+     </mdc-drawer-list>
+   </mdc-drawer>
+
+   <main>
+     <router-view/>
+   </main>
+ </mdc-layout-app>
+
 </template>
 
 <style lang="scss">
+@import "~normalize.css/normalize.css";
+
+$mdc-theme-primary: #212121;
+$mdc-theme-accent: #41b883;
+$mdc-theme-background: #fff;
+
+@import 'vue-mdc-adapter/dist/styles';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,3 +52,13 @@
   }
 }
 </style>
+
+<script lang="ts">
+import Vue from 'vue';
+import VueMDCAdapter from 'vue-mdc-adapter';
+
+Vue.use(VueMDCAdapter);
+
+export default Vue.extend({
+});
+</script>
---
+++ frontend/vue.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+   css: {
+     loaderOptions: {
+       sass: {
+         includePaths: [path.resolve(__dirname,'node_modules')],
+       },
+     },
+   },
+ };
